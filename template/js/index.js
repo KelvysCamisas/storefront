9bdf79ecde5c83b1d605a99bc1cda4ca8cecbbd3
KelvysCamisas <52542731+KelvysCamisas@users.noreply.github.com>
2019-07-18 13:39:10 -0300

    Update index.js

--- template/js/index.js
+++ template/js/index.js
@@ -20,11 +20,9 @@ if ($main && $main.dataset.import) {
 
 // Alterações personalizadas
 
-//Fim
-
 //Chat WhatsApp
-<script> $('body._ecom-store').append('<a href="https://web.whatsapp.com/send?1=pt_BR&phone=554491808130" target="_blank" id="ws_icon_chat" title="Atendimento via WhatsApp"><img src="https://cdn.e-com.club/s/www.kelvys.com.br/uploads/whatsapp_kelvys.png"></a>');
+$('body._ecom-store').append('<a href="https://web.whatsapp.com/send?1=pt_BR&phone=554491808130" target="_blank" id="ws_icon_chat" title="Atendimento via WhatsApp"><img src="https://cdn.e-com.club/s/www.kelvys.com.br/uploads/whatsapp_kelvys.png"></a>');
 if($(window).width() < 480) {
 $("#ws_icon_chat").attr("href", "https://api.whatsapp.com/send?1=pt_BR&phone=554491808130");
-} </script>
-//Fim
+}
+
